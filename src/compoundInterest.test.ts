74745053115685c4864c83b36ad062d881cfeea7
Carlos Vega <cvegah01@gmail.com>
2023-02-19 15:39:34 -0300

    tdd

--- src/compoundInterest.test.ts
+++ src/compoundInterest.test.ts
@@ -19,7 +19,6 @@ describe('compoundInterest', () => {
         expect(interest).toBeCloseTo(expectedInterest);
     });
 
-
     it('should throw an error when the principal is less than 50', () => {
         const principal = 40;
         const rate = 5;
@@ -27,5 +26,14 @@ describe('compoundInterest', () => {
         expect(() => {
             compoundInterest(principal, rate, time);
         }).toThrowError('Principals below 50 are not allowed');
+    });
+
+    it('should throw an error when the principal between 50 and 1000 the rate is not 1', () => {
+        const principal = 50;
+        const rate = 5;
+        const time = 10;
+        expect(() => {
+            compoundInterest(principal, rate, time);
+        }).toThrowError('Principals between 50 and 1000 must have a rate equal to 1');
     });
 });
