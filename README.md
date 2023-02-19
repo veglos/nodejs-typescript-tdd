fbe3b5ddadd24e4824fbdc8ada8108314218ceb4
Carlos Vega <cvegah01@gmail.com>
2023-02-19 15:31:35 -0300

    tdd

---
+++ README.md
@@ -0,0 +1,6 @@
+# Business rules
+
+principals below 50 are not allowed
+principals between 50 and 1000 the rate must be 1
+principals between 1000 and 10000 the rate must be 3
+principals over 10000 the rate must be 5
--- src/compoundInterest.test.ts
+++ src/compoundInterest.test.ts
@@ -18,4 +18,14 @@ describe('compoundInterest', () => {
         const interest = compoundInterest(principal, rate, time);
         expect(interest).toBeCloseTo(expectedInterest);
     });
+
+
+    it('should throw an error when the principal is less than 50', () => {
+        const principal = 40;
+        const rate = 5;
+        const time = 10;
+        expect(() => {
+            compoundInterest(principal, rate, time);
+        }).toThrowError('Principals below 50 are not allowed');
+    });
 });
