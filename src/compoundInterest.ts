d665900ca5d6a6e97cd4cb029f9359dbfef27ec1
Carlos Vega <cvegah01@gmail.com>
2023-02-19 15:32:36 -0300

    tdd

--- src/compoundInterest.ts
+++ src/compoundInterest.ts
@@ -1,4 +1,8 @@
 export function compoundInterest(principal: number, rate: number, time: number): number {
+    if (principal < 50) {
+        throw new Error('Principals below 50 are not allowed');
+    }
+
     const amount = principal * Math.pow(1 + rate / 100, time);
     const interest = amount - principal;
     return interest;
